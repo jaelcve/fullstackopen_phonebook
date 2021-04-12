1738a2287af0bf19c69f49d0d86fbf3600f9ddad
Jian <jianhuang1314@gmail.com>
2021-04-11 22:04:53 -0400

    added personSchema module, created mongo.js, updated httpRequests with mongo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const morgan = require('morgan')
 const express = require('express')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
@@ -44,34 +46,43 @@ morgan.token('body', (req, res) => {
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+        .catch(error => next(error))
 })
 
 app.get('/api/info', (req, res) => {
-    const numberOfPersons = persons.length;
-    res.send(`<p>Phonebook has info for ${numberOfPersons} people<br/><p>${new Date()}</p>`)
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people<br/><p>${new Date()}</p>`)
+    })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    person ? res.json(person) : res.status(404).end()
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                console.log('iserson')
+                res.json(person)
+            } else {
+                console.log('dsds')
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-
-    const person = {
-        id: Math.floor(Math.random() * 1000000),
-        name: body.name,
-        number: body.number
-    }
 
     if (!body.name || !body.number) {
         return res.status(400).json({
@@ -79,28 +90,51 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    duplicateName = persons.find(person => person.name === body.name);
-
-    if (duplicateName) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    logger(req, res , err => {
-        if(err) return console.log('err', err)
-
-        persons = persons.concat(person)
-        res.json(person)
+    const person = new Person({
+        name: body.name,
+        number: body.number
     })
 
+    logger(req, res, err => {
+        if (err) return console.log('err', err)
+
+        person.save().then(savedPerson => {
+            res.json(savedPerson)
+        })
+            .catch(error => next(error))
+    })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 
 })
 
+const errorHandler = (error, req, res, next) => {
+    console.log('error', error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
