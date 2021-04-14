2e7f9c8c3e86ebc1170807895adb4e537b4d0b8f
Jian <jianhuang1314@gmail.com>
2021-04-13 22:08:12 -0400

    add eslint

--- index.js
+++ index.js
@@ -10,131 +10,109 @@ app.use(express.static('build'))
 
 app.use(express.json())
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    }
-    , {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    }
-    , {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
 app.use(morgan('tiny', {
-    skip: (req, res) => {
-        return req.method === 'POST'
-    }
+  skip: (req) => {
+    return req.method === 'POST'
+  }
 }))
 
-morgan.token('body', (req, res) => {
-    return JSON.stringify(req.body)
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
 })
 
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
-    })
-        .catch(error => next(error))
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+    .catch(error => next(error))
 })
 
-app.get('/api/info', (req, res) => {
-    Person.find({}).then(persons => {
-        res.send(`<p>Phonebook has info for ${persons.length} people<br/><p>${new Date()}</p>`)
-    })
-        .catch(error => next(error))
+app.get('/api/info', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people<br/><p>${new Date()}</p>`)
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                console.log('iserson')
-                res.json(person)
-            } else {
-                console.log('dsds')
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        console.log('iserson')
+        res.json(person)
+      } else {
+        console.log('dsds')
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'The name or number is missing'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'The name or number is missing'
     })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
 
-    logger(req, res, err => {
-        person.save()
-            .then(savedPerson => res.json(savedPerson))
-            .catch(error => next(error))
-    })
+  logger(req, res, () => {
+    person.save()
+      .then(savedPerson => res.json(savedPerson))
+      .catch(error => next(error))
+  })
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.log('error', error.message)
+  console.log('error', error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
